d240e8163d2941f4de84dce1206fe34872d61875
Juan De Jesus <j.dejesus@uniandes.edu.co>
2025-08-23 20:47:57 -0500

    Adds drop down list modules

--- src/components/drop-down/drop-down.tsx
+++ src/components/drop-down/drop-down.tsx
@@ -1,10 +1,13 @@
-import React, { SyntheticEvent, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from "./drop-down.module.css"
 import {isStringEmpty}  from "../../shared/utils"
 
-export const DropDownList:React.FC = () => {
+export type DropDownListProps = Partial<HTMLInputElement>
+
+export const DropDownList:React.FC<DropDownListProps> = ({name}) => {
 
     const [value, setValue] = useState<string>("");
+    const [option, setOption] = useState<string | null>("");
     const domRef = useRef(null)
 
     const inputOnChange = (event:React.ChangeEvent<HTMLInputElement> | undefined) => {
@@ -14,15 +17,15 @@ export const DropDownList:React.FC = () => {
     const isOptionListVisible = value.length > 0 && !isStringEmpty(value);
     
     return (
-        <div>
-            <input onChange={inputOnChange} value={value} className={styles["drop-down"]} type="text" />
+        <div className={styles["drop-down-container"]}>
+            <input name={name} onChange={inputOnChange} value={value} className={styles["drop-down"]} type="text" />
             {
                 isOptionListVisible &&
-                <ul ref={domRef}>
-                    <li>Lorep Ipsum</li>
-                    <li>Lorep Ipsum</li>
-                    <li>Lorep Ipsum</li>
-                    <li>Lorep Ipsum</li>
+                <ul ref={domRef} className={styles["drop-down__options"]}>
+                    <li className={styles["drop-down__option"]}>Lorep Ipsum</li>
+                    <li className={styles["drop-down__option"]}>Lorep Ipsum</li>
+                    <li className={styles["drop-down__option"]}>Lorep Ipsum</li>
+                    <li className={styles["drop-down__option"]}>Lorep Ipsum</li>
                 </ul>
             }
         </div>
